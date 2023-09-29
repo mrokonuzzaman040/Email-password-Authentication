b0db97894a1aae26def25dc22035c8a8be334615
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-09-29 20:53:58 +0600

    Updateing Components

--- src/components/Header/Navbar/Navbar.jsx
+++ src/components/Header/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
@@ -38,7 +39,7 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className="navbar-end">
-                <a className="btn">Login</a>
+                <Link to={'/login'} className="btn">Login</Link>
             </div>
         </div>
     );
