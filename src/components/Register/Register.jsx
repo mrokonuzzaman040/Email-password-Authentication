a8519c0aee1d4075e374de4860c5e64b5d72b366
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-09-29 22:18:07 +0600

    Working on register page for valitaion checking

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,12 +1,36 @@
-import React from 'react';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import auth from '../../Firebase/firebase.config';
 
 const Register = () => {
 
+    const [registerError, setRegisterError] = useState('')
+    const [usercreated, setUserCreated] = useState('')
+
     const handelRegister = (e) => {
+
+        //check error
+        setRegisterError('');
+        setUserCreated('');
+
         e.preventDefault();
         const email = e.target[0].value;
         const password = e.target[1].value;
         console.log(email, password);
+
+
+        if (password.length < 6) {
+            setRegisterError('Password Must be at least 6 Characters');
+            return;
+        }
+        //create user with email and password
+        createUserWithEmailAndPassword(auth, email, password)
+            .then((userCredential) => {
+                setUserCreated(userCredential.user);
+            })
+            .catch((error) => {
+                setRegisterError(error.message);
+            });
     }
 
     return (
@@ -15,14 +39,21 @@ const Register = () => {
                 <h2 className='text-2xl font-bold uppercase'>Please Register First</h2>
             </div>
             <form onSubmit={handelRegister} className=''>
-                <input type="email" placeholder="Email Address" className="input input-bordered input-info w-full max-w-xs" />
+                <input required type="email" placeholder="Email Address" className="input input-bordered input-info w-full max-w-xs" />
                 <br />
                 <br />
-                <input placeholder="Password" className='input input-bordered input-info w-full max-w-xs' type="password" name="password" id="" />
+                <input required placeholder="Password" className='input input-bordered input-info w-full max-w-xs' type="password" name="password" id="" />
                 <br />
                 <br />
                 <input className='btn btn-accent' type="submit" value="Register" />
             </form>
+            {
+                registerError && <p className='text-red-500'>{registerError}</p>
+            }
+
+            {
+                usercreated && <p className='text-green-500'>User Created Successfully</p>
+            }
         </div>
     );
 };
