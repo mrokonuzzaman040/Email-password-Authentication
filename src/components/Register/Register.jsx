bcaf9611f518be1fade341c405389e6107be3300
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-09-29 21:31:05 +0600

    Firebase update

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,16 +1,24 @@
 import React from 'react';
 
 const Register = () => {
+
+    const handelRegister = (e) => {
+        e.preventDefault();
+        const email = e.target[0].value;
+        const password = e.target[1].value;
+        console.log(email, password);
+    }
+
     return (
         <div>
             <div className="m-5">
                 <h2 className='text-2xl font-bold uppercase'>Please Register First</h2>
             </div>
-            <form className=''>
-                <input type="text" placeholder="Email Address" className="input input-bordered input-info w-full max-w-xs" />
+            <form onSubmit={handelRegister} className=''>
+                <input type="email" placeholder="Email Address" className="input input-bordered input-info w-full max-w-xs" />
                 <br />
                 <br />
-                <input  placeholder="Password" className='input input-bordered input-info w-full max-w-xs' type="password" name="password" id="" />
+                <input placeholder="Password" className='input input-bordered input-info w-full max-w-xs' type="password" name="password" id="" />
                 <br />
                 <br />
                 <input className='btn btn-accent' type="submit" value="Register" />
