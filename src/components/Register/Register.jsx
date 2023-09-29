66e625d3559ba2d4e340d70fe42f55e786ddd36c
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-09-29 23:09:58 +0600

    Reg page done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,11 +1,14 @@
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 import React, { useState } from 'react';
 import auth from '../../Firebase/firebase.config';
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
 
 const Register = () => {
 
-    const [registerError, setRegisterError] = useState('')
-    const [usercreated, setUserCreated] = useState('')
+    const [registerError, setRegisterError] = useState('');
+    const [usercreated, setUserCreated] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
+    const [terms, setTerms] = useState(false);
 
     const handelRegister = (e) => {
 
@@ -16,11 +19,15 @@ const Register = () => {
         e.preventDefault();
         const email = e.target[0].value;
         const password = e.target[1].value;
-        console.log(email, password);
+        const terms = e.target[2].checked;
+        console.log(email, password, terms);
 
 
         if (password.length < 6) {
             setRegisterError('Password Must be at least 6 Characters');
+            return;
+        } else if (!terms) {
+            setRegisterError('Please Agree to the terms and conditions');
             return;
         }
         //create user with email and password
@@ -34,19 +41,43 @@ const Register = () => {
     }
 
     return (
-        <div>
+        <div className=''>
             <div className="m-5">
                 <h2 className='text-2xl font-bold uppercase'>Please Register First</h2>
             </div>
-            <form onSubmit={handelRegister} className=''>
-                <input required type="email" placeholder="Email Address" className="input input-bordered input-info w-full max-w-xs" />
-                <br />
-                <br />
-                <input required placeholder="Password" className='input input-bordered input-info w-full max-w-xs' type="password" name="password" id="" />
+            <form onSubmit={handelRegister}>
+                <div className="">
+                    <input
+                        required
+                        type="email"
+                        placeholder="Email Address"
+                        className="input input-bordered input-info w-1/2" />
+                    <br />
+                    <br />
+                    <div className="relative">
+                        <input required
+                            placeholder="Password"
+                            className='relative input input-bordered input-info w-1/2'
+                            type={showPassword ? "text" : "password"}
+                            name="password"
+                            id=""
+                        />
+
+                        <span className='absolute top-4 right-60' onClick={() => setShowPassword(!showPassword)}>
+                            {
+                                showPassword ? <AiFillEyeInvisible /> : <AiFillEye />
+                            }
+                        </span>
+                    </div>
+                    <br />
+                    <input type="checkbox" name="terms" id="terms" />
+                    <label htmlFor="terms" className='ml-2'>I agree to the terms and conditions</label>
+                </div>
                 <br />
                 <br />
                 <input className='btn btn-accent' type="submit" value="Register" />
             </form>
+
             {
                 registerError && <p className='text-red-500'>{registerError}</p>
             }
