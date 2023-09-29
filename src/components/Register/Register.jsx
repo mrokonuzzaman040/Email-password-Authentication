96d070dbe70e2f8fdd183cb7c960eeca56d0e259
mrokonuzzaman040 <19201103040@cse.bubt.edu.bd>
2023-09-29 21:16:01 +0600

    Register From design update

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const Register = () => {
     return (
         <div>
-            Register Page
+            <div className="m-5">
+                <h2 className='text-2xl font-bold uppercase'>Please Register First</h2>
+            </div>
+            <form className=''>
+                <input type="text" placeholder="Email Address" className="input input-bordered input-info w-full max-w-xs" />
+                <br />
+                <br />
+                <input  placeholder="Password" className='input input-bordered input-info w-full max-w-xs' type="password" name="password" id="" />
+                <br />
+                <br />
+                <input className='btn btn-accent' type="submit" value="Register" />
+            </form>
         </div>
     );
 };
